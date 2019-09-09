529f01dd6ff82a21cdec651d07be62e28824c501
dpatanin <daniel.patanin@iteratec.com>
2019-09-09 15:45:50 +0200

    resolved warning in getStarted/index.js and clean up

--- src/pages/getStarted/index.js
+++ src/pages/getStarted/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link, graphql } from 'gatsby';
+import { graphql } from 'gatsby';
 import SEO from '../../components/SEO';
 import Layout from '../../components/Layout';
 
 const GetStarted = (props) => {
-  const getStarted = props.data.allMarkdownRemark.edges;
+  const tutorials = props.data.allMarkdownRemark.edges;
   return (
     <Layout bodyClass="page-getStarted">
       <SEO title="Services" />
@@ -13,26 +13,24 @@ const GetStarted = (props) => {
           <div className="row">
             <div className="col-12">
               <h1>Tutorials</h1>
+              <h2>For starting up things</h2>
+              <p>
+                Guides on how to start up engine, scanner, persistence provider
+                and do scan jobs etc.. Perhaps with screencasts.
+              </p>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="container pb-6">
-        {/* <div className="row">
-          {getStarted.map(edge => (
-            <div key={edge.node.frontmatter.path} className="col-12 col-md-4 mb-1">
-              <div className="card service service-teaser">
-                <div className="card-content">
-                  <h2>
-                    <Link to={edge.node.frontmatter.path}>{edge.node.frontmatter.title}</Link>
-                  </h2>
-                  <p>{edge.node.excerpt}</p>
-                </div>
-              </div>
+      <div className="container  pb-2  pb-md-3">
+        <div className="row justify-content-center">
+          {tutorials.map(tutorial => (
+            <div className="feature">
+              <h1>{tutorial.node.frontmatter.title}</h1>
             </div>
           ))}
-        </div> */}
+        </div>
       </div>
     </Layout>
   );
