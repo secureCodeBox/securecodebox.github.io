be5c9dfb4a8934d5dfa13515c17cc4b9cc4ddb68
dpatanin <daniel.patanin@iteratec.com>
2019-09-16 16:55:35 +0200

    make sidebar sticky and fill

--- src/templates/scanner.js
+++ src/templates/scanner.js
@@ -9,64 +9,61 @@ const Scanner = ({ data }) => {
 
   return (
     <Layout bodyClass="page-scanners">
-          <div class="sidebar-wrapper">
-            <nav id="sidebar">
-              <div class="sidebar-header">
-                <h3>Scanner</h3>
-                <strong>Scanner</strong>
-              </div>
+      <div className="sidebar-wrapper">
+        <nav className="sidebar">
+          <ul class="list-unstyled components">
+            {scanner.map(scanner => (
+              <li>
+                <Link to={scanner.node.frontmatter.path}>
+                  {scanner.node.frontmatter.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
 
-              <ul class="list-unstyled components">
-                {scanner.map(scanner => (
-                  <li>
-                  <Link to={scanner.node.frontmatter.path}>{scanner.node.frontmatter.title}</Link>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-
-            <div id="content">
-              <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                  <h1 className="title">{title}</h1>
-                  <div
-                    className="content"
-                    dangerouslySetInnerHTML={{ __html: html }}
-                  />
-                </div>
-              </nav>
+        <div id="content">
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+              <h1 className="title">{title}</h1>
+              <div
+                className="content"
+                dangerouslySetInnerHTML={{ __html: html }}
+              />
             </div>
-          </div>
+          </nav>
+        </div>
+      </div>
     </Layout>
   );
 };
 
 export const query = graphql`
-         query($id: String!) {
-           markdownRemark(id: { eq: $id }) {
-             frontmatter {
-               title
-             }
-             html
-           }
-           allMarkdownRemark(
-             filter: { frontmatter: { category: { eq: "scanner" } } }
-             sort: { fields: [frontmatter___title], order: ASC }
-           ) {
-             edges {
-               node {
-                 html
-                 frontmatter {
-                   title
-                   path
-                   category
-                   usecase
-                   release
-                 }
-               }
-             }
-           }
-         }
-       `;
+  query($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        title
+      }
+      html
+    }
+    allMarkdownRemark(
+      filter: { frontmatter: { category: { eq: "scanner" } } }
+      sort: { fields: [frontmatter___title], order: ASC }
+    ) {
+      edges {
+        node {
+          html
+          frontmatter {
+            title
+            path
+            category
+            usecase
+            release
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default Scanner;
