8ee63e3bf1ac60082d5a8f57430fa4dbbb7c5d8a
Sven Strittmatter <sven.strittmatter@iteratec.com>
2020-07-13 11:55:42 +0200

    Remove contact form
    We used Mailchimp which sets some cookies. So we had the choice
    to add some GDPR stuff or simply remove the contact form.
    
    We chosne the latter option because contact forms are so 90s
    and the last one contacted us by contatc form was over a year
    ago.
    
    Signed-off-by: Sven Strittmatter <sven.strittmatter@iteratec.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,9 +15,6 @@ const Footer = props => (
                 <Link to="/">Home</Link>
               </li>
               <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-              <li>
                 <a href="https://www.iteratec.de/impressum/">Imprint</a>
               </li>
               <li>
