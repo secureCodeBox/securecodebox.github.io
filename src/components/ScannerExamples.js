108fe1f9ce6e6ffba2c453dec0b2021c7112099a
Yannik Fuhrmeister <yannik.fuhrmeister@iteratec.com>
2020-08-12 10:43:35 +0200

    Fix view for scan.yaml

--- src/components/ScannerExamples.js
+++ src/components/ScannerExamples.js
@@ -3,9 +3,12 @@ import { useStaticQuery, graphql } from "gatsby"
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
+const PREFIX = 23
 export default function ScannerExamples(props) {
 
-    const scanner = 'nmap';
+    console.log(props.scanner);
+
+    const scanner = props.scanner.substring(PREFIX);
     const query = graphql`
             query {
                 examples: allFile(filter: {base: {regex: "/^(findings|scan).yaml/"}, extension: {eq: "yaml"}}) {
@@ -25,43 +28,36 @@ export default function ScannerExamples(props) {
                 }
             }`;
     const data = useStaticQuery(query);
-    console.log("Results for query");
-    console.log(data);
     const targets = [];
-    const examples = []; 
+    const examples = [];
 
     const length = data.examples.edges.length
     let i;
     for (i = 0; i < length; i++) {
         const edge = data.examples.edges[i];
         const node = data.examples.nodes[i];
-        console.log(edge.node.relativeDirectory)
         if (edge.node.relativeDirectory.includes(scanner)) {
             const target = data.examples.nodes[i].fields.scanTarget
-            if (!targets.includes(target)) {
+            if(!targets.includes(target))
                 targets.push(target);
-                examples.push(node);
-            }
+            examples.push(node);
         }
     }
-
-    console.log(targets);
-    console.log(examples);
 
     return (
         <div className="container-fluid">
             <h2 className="title">Examples</h2>
-
             <Tabs>
                 <TabList>
                     {targets.map(target => (<Tab key={target}>{target}</Tab>))}
                 </TabList>
                 {targets.map(target => (<TabPanel key={target}>
-                    <Tabs> <TabList>
-                        {examples.reverse().filter(example => example.fields.scanTarget === target).map((example, index) => (
-                            <Tab key={index}>{example.fields.fileName}</Tab>
-                        ))}
-                    </TabList>
+                    <Tabs>
+                        <TabList>
+                            {examples.filter(example => example.fields.scanTarget === target).map((example, index) => (
+                                <Tab key={index}>{example.fields.fileName}</Tab>
+                            ))}
+                        </TabList>
 
                         {examples.filter(example => example.fields.scanTarget === target).map((example, index) => (
                             <TabPanel key={index}>
