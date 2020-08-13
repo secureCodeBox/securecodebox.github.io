efdb192117997fd178e10665d39ea9f3a0cee08b
dpatanin <daniel.patanin@iteratec.com>
2020-08-13 14:00:22 +0200

    make sidebar menus toggable

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,49 @@
 import React, { Fragment } from 'react';
 import { Link } from 'gatsby';
 
+// Toggles sidebar menus, only one may be open at a time
+function toggleMenu(id) {
+  const element = document.getElementById(id);
+  const scanners = document.getElementById('Scanners');
+  const persistenceProviders = document.getElementById('Persistence Providers');
+  const hooks = document.getElementById('Hooks');
+
+  switch (element) {
+    case scanners:
+      persistenceProviders.style.display = 'none';
+      hooks.style.display = 'none';
+
+      element.style.display === 'block'
+        ? (element.style.display = 'none')
+        : (element.style.display = 'block');
+      break;
+
+    case persistenceProviders:
+      scanners.style.display = 'none';
+      hooks.style.display = 'none';
+
+      element.style.display === 'block'
+        ? (element.style.display = 'none')
+        : (element.style.display = 'block');
+      break;
+
+    case hooks:
+      scanners.style.display = 'none';
+      persistenceProviders.style.display = 'none';
+
+      element.style.display === 'block'
+        ? (element.style.display = 'none')
+        : (element.style.display = 'block');
+      break;
+
+    default:
+      scanners.style.display = 'none';
+      persistenceProviders.style.display = 'none';
+      hooks.style.display = 'none';
+      break;
+  }
+}
+
 // Determine the naming of Sidebar categories
 function getCategoryTitle(category) {
   let categoryTitle = '';
@@ -11,11 +54,13 @@ function getCategoryTitle(category) {
       break;
 
     case 'hook':
-        category[0].node.frontmatter.type === 'persistenceProvider' ? categoryTitle = 'Persistence Providers' : categoryTitle = 'Hooks';
+      category[0].node.frontmatter.type === 'persistenceProvider'
+        ? (categoryTitle = 'Persistence Providers')
+        : (categoryTitle = 'Hooks');
       break;
 
     default:
-        categoryTitle = 'Unknown Category';
+      categoryTitle = 'Unknown Category';
       break;
   }
 
@@ -27,8 +72,16 @@ const Sidebar = (props) => {
     <nav className="sidebar">
       {props.dataArray.map((category, index) => (
         <Fragment key={index}>
-          <h1 className="sidebar-header">{getCategoryTitle(category)}</h1>
-          <ul className="list-unstyled components">
+          <h1
+            onClick={() => toggleMenu(getCategoryTitle(category))}
+            className="sidebar-header"
+          >
+            {getCategoryTitle(category)}
+          </h1>
+          <ul
+            id={getCategoryTitle(category)}
+            className="list-unstyled components show"
+          >
             {category.map((element) => (
               <li key={element.node.frontmatter.title}>
                 <Link
